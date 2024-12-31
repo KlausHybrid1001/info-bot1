2ee14146306a504c3b251ab8dde3e4e90f8898db
KlausHybrid1001 <107557518+KlausHybrid1001@users.noreply.github.com>
2025-01-01 01:51:24 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+
+# Set the Telegram webhook
+echo "Setting Telegram Webhook..."
+curl -s "https://api.telegram.org/bot7597041420:AAGxS7T7fnwenj1FvlpR1bEl5niRm_tCAzU/setWebhook?url=https://info-bot1-1.onrender.com/webhook/7597041420:AAGxS7T7fnwenj1FvlpR1bEl5niRm_tCAzU"
+
+# Start the Python bot
+echo "Starting Python bot..."
 python bot.py
